0b6bf97b2d7bd5fcb710e70c8e0f28608ca7d7da
Tri Nguyen <tri@tridnguyen.com>
2014-07-22 14:23:04 -0400

    use templates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+/* global Templates */
+
+'use strict';
+
+jQuery('document').ready( function ($) {
+    var template = Templates.item();
+    console.log(template);
+});
