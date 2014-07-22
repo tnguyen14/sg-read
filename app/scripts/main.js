eded96c1b8ddfc2e4843facfc21a232869b22b43
Tri Nguyen <tri@tridnguyen.com>
2014-07-22 17:24:07 -0400

    get an article and display the returned resource from embedly

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,22 +2,37 @@
 
 'use strict';
 
-var url = '/* @echo url */' || 'http://localhost:3030';
+var serverUrl = '/* @echo url */' || 'http://localhost:3030';
 
 jQuery('document').ready( function ($) {
-    var template = Templates.item;
+    var itemTemplate = Templates.item,
+        resourceTemplate = Templates.resource;
     $.ajax({
-        url: url,
+        url: serverUrl,
         success: function (articles) {
             _.each(articles, function (article) {
-                $('.shared-articles').append(template(article));
+                $('.shared-articles').append(itemTemplate(article));
             });
         }
     });
     $('[name="sharer"]').on('change', function () {
         if ($(this).val() === 'other') {
-            $('.sharer-container').removeClass('col-sm-12').addClass('col-sm-6');
+            $('.sharer-container').removeClass('col-sm-6').addClass('col-sm-6');
             $('.sharer-name-container').show();
         }
+    });
+    $('[name="url"]').on('change', function () {
+        var url = $(this).val();
+        if (url === '') {
+            $('.resource').empty();
+            return;
+        }
+        $.ajax({
+            url: serverUrl + '/extract/' + encodeURIComponent(url),
+            success: function (data) {
+                console.log(data);
+                $('.resource').html(resourceTemplate(data));
+            }
+        });
     });
 });
